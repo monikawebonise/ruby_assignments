dab1440ceb6a7e2ba6bcdcae257491d3bf29e7dd
monikawebonise <smart.monika1@gmail.com>
2015-03-10 09:56:34 +0530

    rubyBasicASgnComplete

---
+++ rubyBasicAsgn2/array.rb
@@ -0,0 +1,17 @@
+#initialising a array
+days_of_week = Array[ "Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun" ]
+puts days_of_week
+
+#getting the size of array
+puts days_of_week.size
+
+#getting type of elements in strings
+puts days_of_week[0].class
+
+#retrieving a few elements
+puts days_of_week[0, 3]
+
+#slice of a few elements
+puts days_of_week.slice(1..3)
+
+
--- rubyBasicAsgn2/string.rb
+++ rubyBasicAsgn2/string.rb
@@ -9,3 +9,22 @@ str1="abc"
 str2="ABC"
 ans=str1 <=> str2
 puts ans
+
+#inserting into strings
+myString = "Paris in Spring"
+puts myString
+myString.insert 8, " the"
+
+#reversing string
+puts myString
+revString=myString.reverse
+puts revString
+#=> "gnirpS eht ni siraP"
+
+#substitution in string
+myString = "Welcome to PHP Essentials!"
+puts myString
+
+substString=myString.gsub("PHP", "Ruby")
+#=> "Welcome to Ruby Essentials!"
+puts substString
